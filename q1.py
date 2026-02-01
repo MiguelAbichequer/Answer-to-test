748be75d8f78bfb91cc071ed5948761202127ee3
MiguelAbichequer <abichequerm@gmail.com>
2026-02-01 15:54:16 -0300

    Mudanças

--- q1.py
+++ q1.py
@@ -1,7 +1,82 @@
 import pandas as pd
 import zipfile
 import os
+import requests
+from bs4 import BeautifulSoup
 
+def achar_link(url):
+    requisicao = requests.get(url)
+    dados_pagina = BeautifulSoup(requisicao.text, 'html.parser')
+    link = dados_pagina.find_all("a", href=lambda h: h and "demonstracoes_contabeis" in h)
+    # encontra todas as ocorrências de links que contenham "demonstracoes_contabeis"
+    
+    link = url + link[0]['href']
+    return link
+
+def baixar_arquivo(url, endereco):
+
+    requisicao = requests.get(url)
+    # acessa o link encontrado para a pasta correspondente
+
+    with open(endereco, 'wb') as novo_arquivo:
+        novo_arquivo.write(requisicao.content)
+
+def preparar_arquivos(url):
+
+    requisicao = requests.get(url)
+    # obter informações contidas na pasta do ano selecionado
+
+    dados_pagina = BeautifulSoup(requisicao.text, 'html.parser')
+
+    arquivos = dados_pagina.find_all("a", href=lambda h: h and h.endswith(".zip"))
+
+    lista_nomes = []
+
+    count = 0 # para pegar apenas os últimos 3 trimestres
+    for arq in arquivos:
+        lista_nomes.append(arq['href']) # salva os nomes dos arquivos que serão baixados e descompactados.
+        count = count + 1
+        if count == 2:
+            break
+
+
+    # 1 - baixar arquivos
+
+    for nome in lista_nomes:
+        link = url + nome
+        endereco = nome
+        baixar_arquivo(link, endereco)
+
+    
+        
+
+
+
+
+def selecionar_ano(url):
+
+    requisicao = requests.get(url)
+    dados_pagina = BeautifulSoup(requisicao.text, 'html.parser')
+
+    anos = dados_pagina.find_all("a") # para separar os links correspondetes a cada um dos anos.
+
+    lista = []
+    
+
+    for ano in anos:
+        if(ano['href'].startswith(("20", "19"))): # para evitar valores lixo, separamos somente os anos.
+            lista.append(int(ano['href'][:4])) # cortar a string para seperar somente o ano.
+    
+    ano_mais_recente = max(lista) # o ano mais recente é também o maior de todos eles.
+
+    ano_mais_recente = str(ano_mais_recente) + '/' # para concataner e formar o link
+
+    link = url + ano_mais_recente
+
+    return link
+
+
+        
 
 def buscar(df):
     if 'DESCRICAO' not in df.columns:
@@ -10,15 +85,14 @@ def buscar(df):
     DES = df[df['DESCRICAO'] == "Despesas com Eventos/Sinistros - Judicial"]  # filtro coluna DESCRICAO
     return DES
 
-def dados(nome_arquivo):
+
+def descompacta_e_filtra(nome_arquivo):
     zip_path = nome_arquivo
     zip_dir = "extractedFiles"
 
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(zip_dir)
         for nome_interno in zip_ref.namelist():
-
-        
 
             print(nome_interno)
             new_csv = pd.DataFrame()
@@ -34,15 +108,26 @@ def dados(nome_arquivo):
             else:
                 print("Formato de arquivo inválido")
 
-        return new_csv
-
-resultado = dados("1T2025.zip")
-
-print(resultado)
+        if not new_csv.empty:
+            return new_csv
+        
 
 
+###
+
+###
 
 
+if __name__ == "__main__":
+    url = "https://dadosabertos.ans.gov.br/FTP/PDA/"
+
+    url_doc = achar_link(url)
+
+    url_ano = selecionar_ano(url_doc)
+
+    preparar_arquivos(url_ano)
+
+    
 
 
 
